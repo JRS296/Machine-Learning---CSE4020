0f465219da68d2e3aa4c78458b8d6a732daeb4d5
JRS296 <jonathansamuel296@gmail.com>
2022-01-22 19:37:05 +0530

    Initial Commit #0 - 22.01.2022 @19:36

---
+++ Lab Class/Day 1 - 6.1.2022/hello.py
@@ -0,0 +1,6 @@
+import pandas as pd
+data = {'Reg No': ['20BCt0330', '20BCt0331', '20BCt0332'], 'Age': [
+    20, 21, 19], 'M1': [3, 6, 9], 'M2': [4, 8, 0], 'M3': [3, 1, 2], 'Total': [9, 6, 4]}
+df = pd.DataFrame(data)
+
+print(df)
