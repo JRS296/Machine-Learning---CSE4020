ac0ba72de1ece849045646b92a92e7b4b9b88b0f
JRS296 <jonathansamuel296@gmail.com>
2022-03-10 22:36:14 +0530

    Commit #7 - Completion of Work - 10.03.2022 @22:36

--- Lab Assignment - 3/q2.py
+++ Lab Assignment - 3/q2.py
@@ -1,46 +1,25 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sklearn.model_selection import train_test_split
+import pandas as pd
 from sklearn.neural_network import MLPClassifier
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-def acuracy_score(predictions, test):
-	correct = 0
-	for i in range(len(test)):
-		if test[i][-1] == predictions[i]:
-			correct += 1
-	return (correct / float(len(test))) * 100.0
 
 df = pd.read_csv('Lab Assignment - 3\car_evaluation.csv')
 print(df.head(5)) #Check if CSV file was loaded successfully
+y = df['buying']
+X = df.drop(columns = ['buying'])
+X, y = make_classification(n_samples=1728, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
+clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+clf.predict(X_test)
+clf.score(X_test, y_test)
 
 
-
-X = df.iloc[:,1:-1]
-Y = df.iloc[:,-1].values
-print("Shape of X - ", X.shape)
-print("Shape of Y - ", Y.shape)
-print()
-train_x, test_x, train_y, test_y = train_test_split (X,Y, test_size=0.35, random_state=45)
-print("Test Size taken = 0.35")
-print("Random State taken = 45\n")
-
-m_nb =  MLPClassifier(alpha = 1.9)
-m_nb.fit(train_x, train_y)
-y_predict_NB = m_nb.predict(test_x)
-target_labels = ['Legitimate','Spam']
-print(classification_report(y_predict_NB, test_y, target_names = target_labels))
-print("Accuracy for given MLP Classfier Model: ", accuracy_score(y_predict_NB, test_y))
-
-cm = confusion_matrix(test_y, y_predict_NB)
-grp_names = ['True Negatives','False Positives','False Negatives','True Positives']
-grp_counts = ["{0:0.0f}".format(value) for value in cm.flatten()]
-
-labels = [f"{v1}\n{v2}" for v1, v2, in zip(grp_names,grp_counts)] 
-labels = np.asarray(labels).reshape(2,2)
-sns.heatmap(cm, annot=labels, fmt='', cmap="BuPu")
-plt.show()
-print("Confusion Matrix Values: ",cm)
-
+#car_evaluation = pd.read_csv("Lab Assignment - 3\car_evaluation.csv")
+#X = car_evaluation.drop(columns = ['decision'])
+#y = car_evaluation['decision']
+#X, y = make_classification(n_samples=1728, random_state=1)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=1)
+#clf = MLPClassifier(random_state=1, max_iter=300).fit(X_train, y_train)
+#clf.predict(X_test)
+#clf.score(X_test, y_test)
--- Lab Assignment - 3/test.py
+++ Lab Assignment - 3/test.py
@@ -1,167 +1,47 @@
-from cross_validation import crossvalidation_split
-import math
-import numpy as np
-from random import random
-from math import *
-from flood_dataset import *
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# save activation and derivative
-# implement backpropagation
-# implement gradient descent
-# implement train
-# train our net with some dummy dataset
-# make some prediction
+def acuracy_score(predictions, test):
+	correct = 0
+	for i in range(len(test)):
+		if test[i][-1] == predictions[i]:
+			correct += 1
+	return (correct / float(len(test))) * 100.0
 
-
-class MLP(object):
-
-    def __init__(self, num_inputs=3, hidden_layers=[3, 3], num_outputs=2):
-
-        self.num_inputs = num_inputs
-        self.hidden_layers = hidden_layers
-        self.num_outputs = num_outputs
-
-        layers = [num_inputs] + hidden_layers + [num_outputs]
-
-        # initiate random weight
-        weight = []
-        for i in range(len(layers)-1):
-            w = np.random.rand(layers[i], layers[i+1])
-            weight.append(w)
-        self.weight = weight
-
-        activation = []
-        for i in range(len(layers)):
-            a = np.zeros(layers[i])
-            activation.append(a)
-        self.activation = activation
-
-        derivative = []
-        for i in range(len(layers) - 1):
-            d = np.zeros((layers[i], layers[i + 1]))
-            derivative.append(d)
-        self.derivative = derivative
-
-    def forward_propogate(self, inputs):
-
-        activation = inputs
-        self.activation[0] = activation
-
-        for i, w in enumerate(self.weight):
-            # calculate net inputs
-            net_inputs = np.dot(activation, w)
-
-            # calculate the activation
-            activation = self._sigmoid(net_inputs)
-            self.activation[i+1] = activation
-        return activation
-
-    def back_propagate(self, error, verbose=False):
-
-        for i in reversed(range(len(self.derivative))):
-             # get activation for previous layer
-            activation = self.activation[i+1]
-
-            # apply sigmoid derivative function
-            delta = error * self._sigmoid_derivative(activation)
-
-            # reshape delta as to have it as a 2d array
-            delta_re = delta.reshape(delta.shape[0], -1).T
-
-            # get activations for current layer
-            current_activation = self.activation[i]
-
-            # reshape activations as to have them as a 2d column matrix
-            current_activation = current_activation.reshape(current_activation.shape[0],-1)
-
-            # save derivative after applying matrix multiplication
-            self.derivative[i] = np.dot(current_activation, delta_re)
-
-            # backpropogate the next error
-            error = np.dot(delta, self.weight[i].T)
-
-        return error
+df = pd.read_csv('Lab Assignment - 3\car_evaluation.csv')
+print(df.head(5)) #Check if CSV file was loaded successfully
 
 
-    def train(self, inputs, targets, epochs, learning_rate):
 
-        for i in range(epochs):
-            sum_error = 0
-            for j, input in enumerate(inputs):
-                target = targets[j]
+X = df.iloc[:,1:-1]
+Y = df.iloc[:,-1].values
+print("Shape of X - ", X.shape)
+print("Shape of Y - ", Y.shape)
+print()
+train_x, test_x, train_y, test_y = train_test_split(X,Y, test_size=0.35, random_state=45)
+print("Test Size taken = 0.35")
+print("Random State taken = 45\n")
 
-                # forward prop
-                output = self.forward_propogate(input)
+m_nb =  MLPClassifier(alpha = 1.9)
+m_nb.fit(train_x, train_y)
+y_predict_NB = m_nb.predict(test_x)
+target_labels = ['Legitimate','Spam']
+print(classification_report(y_predict_NB, test_y, target_names = target_labels))
+print("Accuracy for given MLP Classfier Model: ", accuracy_score(y_predict_NB, test_y))
 
-                # calculate error
-                error = target - output
+cm = confusion_matrix(test_y, y_predict_NB)
+grp_names = ['True Negatives','False Positives','False Negatives','True Positives']
+grp_counts = ["{0:0.0f}".format(value) for value in cm.flatten()]
 
-                # back propagation
-                self.back_propagate(error)
-
-                # apply gradient descent
-                self.gradient_descent(learning_rate)
-
-                sum_error += self._mse(target, output)
-
-            # report error
-            print("Error: {} at epoch {}".format(sum_error / len(inputs), i+1))
-
-    print("Training complete!")
-    print("=====")
-
-    def gradient_descent(self, learning_rate=1):
-        for i in range(len(self.weight)):
-            weights = self.weight[i]
-            derivatives = self.derivative[i]
-            weights += derivatives * learning_rate
-
-    def _sigmoid(self, x):
-        y = 1.0 / (1 + np.exp(-x))
-        return y
-
-    def _sigmoid_derivative(self, x):
-        return x * (1.0 - x)
-
-    def _mse(self, target, output):
-        return np.average((target - output)**2)
+labels = [f"{v1}\n{v2}" for v1, v2, in zip(grp_names,grp_counts)] 
+labels = np.asarray(labels).reshape(2,2)
+sns.heatmap(cm, annot=labels, fmt='', cmap="BuPu")
+plt.show()
+print("Confusion Matrix Values: ",cm)
 
 
-if __name__ == "__main__":
-    Input = []
-    Output = []
-    error_log = []
-    k=10
-    folds = crossvalidation_split(Dataset, k)
-
-    for i in range(k):
-        _k = len(folds[i])
-        # create a dataset to train a network for the sum operation
-        items = np.array([folds[i][j][0:len(folds[i][j])-1] for j in range(len(folds[i]))])
-        targets = np.array([folds[i][j][len(folds[i][j])-1] for j in range(len(folds[i]))])
-        
-
-        # create a Multilayer Perceptron with one hidden layer
-        mlp = MLP(8, [8,8], 1)
-
-        # train network
-        mlp.train(items, targets, 1000, 0.8)
-
-        _input = folds[i][_k-1][0:len(folds[i][_k-1])-1]
-        input = np.array(_input)
-        target = np.array((folds[i][_k-1][len(folds[i][_k-1])-1]))
-
-        output = mlp.forward_propogate(input)
-        Input.append(denomallize(target))
-        Output.append(denomallize(output))
-
-        print()
-        print("if 2 station  have water level = {}  \nIn the next 7 hours the water level should be {}".format(denomallize(input[0:8]), denomallize(output)))
-        print("but actually should be {}".format(denomallize(target)))
-        print()
-
-        error_log.append(abs(denomallize(target)-denomallize(output))*100/denomallize(target))
-
-
-    for i in range(10):
-        print("error round {} : {:.2f}%".format(i+1,error_log[i][0]))
